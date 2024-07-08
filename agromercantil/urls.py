e617ae401c2eacfb8c12e68091b0812a24ab7e92
lmos0 <luismarcio.os@outlook.com>
2024-07-07 23:01:37 -0300

    alteração da url do app

--- agromercantil/urls.py
+++ agromercantil/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('dados_financeiros/', include('dados_financeiros.urls'))
+    path('', include('dados_financeiros.urls'))
 ]
